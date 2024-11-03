48dd823179f3554889faffc984ccf002a0f4852d
AceDemo1 <lawal641@gmail.com>
2024-11-03 02:14:08 +0000

    get hyper

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -45,10 +45,10 @@ class Server:
         data_len = len(self.dataset())
         total_page = (data_len + (page_size - 1))  // page_size
         return {
-                "page_size": page_size,
+                "page_size": len(data),
                 "page": page,
                 "data": data,
                 "next_page": page + 1 if page < total_page else None,
                 "prev_page": page - 1 if page > 1 else None,
-                "total_page": total_page 
+                "total_pages": total_page 
                 }
