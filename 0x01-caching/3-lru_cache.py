de99d9542c6ec8100dd8f7f9790912d89297dc9a
AceDemo1 <lawal641@gmail.com>
2024-11-04 21:37:30 +0000

    LIFO cache

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Create LIFO cache"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """define class"""
+
+    def __init__(self):
+        """initialize class"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """put into cache"""
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data.pop(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                key_discarded, _ = self.cache_data.popitem()
+                print(f'DISCARD: {key_discarded}')
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """get value of key"""
+        value = self.cache_data.pop(key)
+        self.cache_data[key] = value
+        return value
