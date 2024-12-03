58f120a6528d7788ab04360184f6a0678f07a36e
AceDemo1 <lawal641@gmail.com>
2024-12-03 12:13:32 +0000

    flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
     def welcome_page() -> str:
-    """hompage"""
-    return render_template('0-index.html')
+        """hompage"""
+        return render_template('0-index.html')
 
 
 if __name__ == '__main__':
