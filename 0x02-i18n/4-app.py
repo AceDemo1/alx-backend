dad419a70273879ac8b49e6a9f3d397493607cfd
AceDemo1 <lawal641@gmail.com>
2024-12-03 17:49:39 +0000

    flask

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -27,7 +27,7 @@ def get_locale():
 @app.route('/')
 def welcome_page() -> str:
     """hompage"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
