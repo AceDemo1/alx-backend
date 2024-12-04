b5ef2a5fc86d37ec88dcd86c30ef38de101ca52f
AceDemo1 <lawal641@gmail.com>
2024-12-04 19:13:00 +0000

    flask

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""simple flask template"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """config file"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@babel.localeselector
+def get_locale():
+    """get lang"""
+    lang = request.args.get('locale')
+    if lang in Config.LANGUAGES:
+        return lang
+    if g.user and g.user.get('locale') in Config.LANGUAGES:
+        return g.user.get('locale')
+    lang = request.accept_languages.best_match(Config.LANGUAGES)
+    if lang:
+        return lang
+    return Config.BABEL_DEFAULT_LOCALE
+
+
+@app.route('/')
+def welcome_page() -> str:
+    """hompage"""
+    return render_template('5-index.html')
+
+
+def get_user():
+    """get user"""
+    usr_id = request.args.get('login_as')
+    try:
+        usr_id = int(usr_id)
+        if usr_id in users:
+            return users.get(usr_id)
+    except BaseException:
+        return None
+
+
+@app.before_request
+def before_request():
+    """run befor others"""
+    g.user = get_user()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000", debug=True)
